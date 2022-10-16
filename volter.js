5c60a4dd5449a7e5781dce80d5c7d7a917fd617d
VanderStore <113655725+VanderStore@users.noreply.github.com>
2022-10-16 20:23:43 +0700

    Update volter.js

--- volter.js
+++ volter.js
@@ -153,7 +153,7 @@ module.exports = volter = async (volter, m, chatUpdate, store) => {
             if (typeof setting !== 'object') global.db.data.settings[botNumber] = {}
 	    if (setting) {
 		if (!isNumber(setting.status)) setting.status = 0
-		if (!('autobio' in setting)) setting.autobio = true
+		if (!('autobio' in setting)) setting.autobio false
 		if (!('templateImage' in setting)) setting.templateImage = true
 		if (!('templateVideo' in setting)) setting.templateVideo = false
 		if (!('templateGif' in setting)) setting.templateGif = false
